e8d4107a4603a33e8b1cfbccfa2faf67dcaef8ce
Luke1982 <gcx3@hotmail.com>
2021-10-07 20:38:50 +0200

    Prevent header from collapsing on scroll

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import {Admin, Resource} from 'react-admin'
+import { Fragment } from 'react'
+import {Admin, Resource, AppBar, Layout} from 'react-admin'
 import { WorkassignmentList } from './components/WorkassignmentList'
 import { WorkassignmentEdit } from './components/WorkassignmentEdit'
 import { authProvider } from './providers/authProvider'
 import { dataProvider } from './providers/dataProvider'
 import { i18nProvider } from './providers/i18nProvider'
 
+const CustomAppBar = props => {
+  return <AppBar {...props} container={Fragment} />
+}
+
+const CustomLayout = props => {
+  return <Layout {...props} appBar={CustomAppBar} />
+}
 
 const App = () => (
     <Admin
-      authProvider={authProvider('http://cbdevelop.local')}
-      dataProvider={dataProvider('http://cbdevelop.local')}
+      authProvider={authProvider('http://cbxcrm.local')}
+      dataProvider={dataProvider('http://cbxcrm.local')}
       i18nProvider={i18nProvider}
+      layout={CustomLayout}
     >
       <Resource
         name="WorkAssignment"
