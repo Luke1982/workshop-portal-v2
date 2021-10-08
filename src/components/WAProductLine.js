686697fea9cce245a748402d4c32532124647fa6
Luke1982 <gcx3@hotmail.com>
2021-10-08 08:30:26 +0200

    First scaffold of workassignmentline support

---
+++ src/components/WAProductLine.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { useDataProvider } from 'ra-core'
+
+const WAProductLine = ({record, products}) => {
+	const dataProvider = useDataProvider()
+	const [productsLoaded, setProductsLoaded] = useState(false)
+	const [product, setProduct] = useState({
+		productname: 'Laden...',
+		product_no: 'Laden...',
+		vendor_part_no: 'Laden...',
+		cf_940: 'Laden...'
+	})
+	const [vendor, setVendor] = useState({
+		vendorname: 'Niet bekend'
+	})
+
+	useEffect(() => {
+		if (!!products) {
+			setProductsLoaded(true)
+			const foundProduct = products.find(p => p.id === record.product)
+			setProduct(foundProduct)
+		}
+	}, [products, record.product])
+
+	useEffect(() => {
+		if (productsLoaded === true && product.vendor_id !== '') {
+			const getVendor = async () => {
+				const retrievedVendor = await dataProvider.getOne('Vendors', {id: product.vendor_id})
+				setVendor(retrievedVendor.data)
+			}
+			getVendor()
+		}
+	}, [productsLoaded, dataProvider, product.vendor_id])
+
+	const markReady = id => {
+		console.log(id)
+	}
+
+	return (
+		<div className="slds-box slds-theme_shade slds-m-bottom_small">
+			<div className="slds-grid">
+				<div className="slds-col">
+					<div className="slds-text-heading_medium">{product.productname}</div>
+				</div>
+				<div className="slds-col">
+					<button className="slds-button slds-button_success slds-float_right" onClick={() => {markReady(record.id)}}>Klaar</button>
+				</div>
+			</div>
+			<div className="slds-grid">
+				<div className="slds-col slds-size_1-of-5">
+					<div className="slds-text-title">Productnummer</div>
+					<div className="slds-text-body_regular">{product.product_no}</div>
+				</div>
+				<div className="slds-col slds-size_1-of-5">
+					<div className="slds-text-title">Artikelnummer</div>
+					<div className="slds-text-body_regular">{product.vendor_part_no}</div>
+				</div>
+				<div className="slds-col slds-size_1-of-5">
+					<div className="slds-text-title">Leverancier</div>
+					<div className="slds-text-body_regular">{vendor.vendorname}</div>
+				</div>
+				<div className="slds-col slds-size_1-of-5">
+					<div className="slds-text-title">Locatie</div>
+					<div className="slds-text-body_regular">{product.cf_940}</div>
+				</div>
+				<div className="slds-col slds-size_1-of-5">
+					<div className="slds-text-title">Aantal</div>
+					<div className="slds-text-body_regular">{record.qty}</div>
+				</div>
+			</div>
+			<div className="slds-grid slds-m-top_medium">
+				<div className="slds-col slds-size_8-of-12">
+					<div className="slds-text-title">Opmerkingen</div>
+					<div className="slds-text-body_regular">{record.description}</div>
+				</div>
+				<div className="slds-size_4-of-12 slds-grid">
+					<div className="slds-form-element slds-size_8-of-12">
+						<label className="slds-form-element__label" for={`serial-${record.id}`}>Serienummer</label>
+						<div className="slds-form-element__control">
+							<input type="text" id={`serial-${record.id}`} placeholder="Voer het serienummer in" className="slds-input" />
+						</div>
+					</div>
+					<div className="slds-size_4-of-12">
+						<button className="slds-button slds-button_brand slds-m-top_large slds-m-left_small">Serienummer opslaan</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default WAProductLine
--- src/components/WorkassignmentContent.js
+++ src/components/WorkassignmentContent.js
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, {useRef, useState, useEffect} from 'react'
+import {Spinner} from '@salesforce/design-system-react'
+import WAProductLine from './WAProductLine'
+import { useDataProvider } from 'ra-core'
 
 const WorkAssignmentContent = ({record}) => {
+	const dataProvider = useDataProvider()
+	const topbarRef = useRef()
+	const [lines, setLines] = useState(false)
+	const [products, setProducts] = useState(false)
+	const [productIds, setProductIds] = useState(false)
+	const [productsLoaded, setProductsLoaded] = useState(false)
+	const [renderedLines, setRenderedLines] = useState([<Spinner
+		key="spinner"
+		variant="brand"
+		containerClassName="slds-is-relative slds-p-vertical_large"
+		/>]
+	)
+
+	window.addEventListener('scroll', () => {
+		requestAnimationFrame(() => {
+			if (topbarRef.current !== undefined) {
+				topbarRef.current.style.transform = `translateY(${window.scrollY}px)`
+			}
+		})
+	})
+
+	useEffect(() => {
+		const getLines = async () => {
+			const result = await dataProvider.getManyReference(
+				'WorkAssignmentLines', {
+					target: 'workassignment',
+					id: record.id,
+					sort: {
+						field: 'seq',
+						order: 'ASC'
+					}
+				}
+			)
+			setLines(result.data)
+		}
+		getLines()
+	}, [dataProvider, record.id])
+
+	useEffect(() => {
+		if (!!lines && !products) {
+			const collectedProductIds = []
+			const renderedLines = lines.map(line => {
+				collectedProductIds.push(line.product)
+				return <WAProductLine record={line} key={line.id} products={products} />
+			})
+			setProductIds(collectedProductIds)
+			setRenderedLines(renderedLines)
+		} else if (productsLoaded === true) {
+			const renderedLines = lines.map(line => {
+				return <WAProductLine record={line} key={line.id} products={products} />
+			})
+			setRenderedLines(renderedLines)
+		}
+	}, [lines, products, productsLoaded])
+
+	useEffect(() => {
+		if (!!productIds) {
+			const getProducts = async () => {
+				if (productIds.length > 0) {
+					const result = await dataProvider.getMany('Products', {ids: productIds})
+					setProducts(result.data)
+					setProductsLoaded(true)
+				}
+			}
+			getProducts()
+		}
+	}, [productIds, dataProvider])
+
 	return (
-		<div className="topbar">Hoi</div>
+		<>
+			<div className="slds-grid slds-box slds-theme_shade slds-theme_alert-texture" ref={topbarRef}>
+				<div className="slds-col">
+					<b>Werbon nummer:</b> {record.workassignment_no}
+				</div>
+				<div className="slds-col">
+					<b>Ordernummer:</b>
+				</div>
+				<div className="slds-col">
+					<b>Klant:</b>
+				</div>
+				<div className="slds-col">
+					<b>Uiterlijk klaar op:</b>
+				</div>
+			</div>
+			<div className="slds-box slds-m-top_small">
+				{renderedLines}
+			</div>
+		</>
 	)
 }
 
